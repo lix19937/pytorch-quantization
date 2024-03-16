f3c853db4a87e490287174c3311d60004d70e594
lix19937 <38753233+lix19937@users.noreply.github.com>
2024-03-16 21:36:04 +0800

    Update histogram.py

--- pytorch_quantization/calib/histogram.py
+++ pytorch_quantization/calib/histogram.py
@@ -297,9 +297,9 @@ def _compute_amax_percentile(calib_hist, calib_bin_edges, percentile):
     if calib_bin_edges is None and calib_hist is None:
         return None
 
-    total = calib_hist.sum()
-    cdf = np.cumsum(calib_hist / total)
-    idx = np.searchsorted(cdf, percentile / 100)
+    total = calib_hist.sum() # 总的数据数目  
+    cdf = np.cumsum(calib_hist / total) # 归一化 
+    idx = np.searchsorted(cdf, v=percentile / 100) # 先对 cdf 升序排序，v插入到合适位置    
     calib_amax = calib_bin_edges[idx]
     calib_amax = torch.tensor(calib_amax.item()) #pylint: disable=not-callable
 
