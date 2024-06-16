29e8ff7ea022bb917e903dabc6828de965bb57a3
lix19937 <38753233+lix19937@users.noreply.github.com>
2024-06-16 11:22:05 +0800

    Update clip_test.py

--- tests/clip_test.py
+++ tests/clip_test.py
@@ -33,15 +33,18 @@ torch.manual_seed(1234)
 
 # pylint:disable=missing-docstring, no-self-use
 
+# 截断   
 
 class TestClip():
 
     def test_simple_run(self):
-        x_np = np.random.rand(1023).astype(np.float32)
+        x_np = np.random.rand(1023).astype(np.float32)        
         x_torch = torch.Tensor(x_np)
+        
         clip_op = clip.Clip(torch.tensor(0.3), torch.tensor(0.7))
+        clip_x_torch = clip_op(x_torch)        
         clip_x_np = np.clip(x_np, 0.3, 0.7)
-        clip_x_torch = clip_op(x_torch)
+        
         np.testing.assert_array_equal(clip_x_torch.cpu().numpy(), clip_x_np)
 
     def test_raise(self):
@@ -50,13 +53,13 @@ class TestClip():
 
     def test_backward(self):
         x = torch.randn(3, 7, requires_grad=True)
-        x.retain_grad()
+        x.retain_grad() #
 
         min_value = 0.3
         max_value = 0.7
         clip_op = clip.Clip(min_value, max_value, learn_min=True, learn_max=True)
         clip_x = clip_op(x)
-        clip_x.retain_grad()
+        clip_x.retain_grad() #  
 
         labels = torch.randint(6, (3,)).type(torch.LongTensor).cuda()
         criterion = torch.nn.CrossEntropyLoss()
